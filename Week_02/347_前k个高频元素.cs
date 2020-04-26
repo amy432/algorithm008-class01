1bae9b4bb50242fda730fbe5737a68ccf30396aa
amy432 <amy4321@qq.com>
2020-04-26 22:19:49 +0800

    第2周作业更新

---
+++ Week_02/347_前k个高频元素.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_347
+{
+    /// <summary>
+    /// 347. 前k个高频元素
+    /// https://leetcode-cn.com/problems/top-k-frequent-elements/
+    /// </summary>
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var sol = new Solution();
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+
+            var result = sol.TopKFrequent(nums,k);
+
+            Console.WriteLine(string.Join(',',result));
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 暴力解法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                    dict[num]++;
+                else
+                    dict[num] = 1;
+            }
+
+            return dict.OrderByDescending(t => t.Value).Take(k).Select(k => k.Key).ToArray();
+        }
+
+    }
+}
---
+++ Week_02/429_N叉树的层序遍历.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace LeetCode_429
+{
+    /// <summary>
+    /// 429. N叉树的层序遍历
+    /// https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+    /// </summary>
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        ///// <summary>
+        ///// 方法1：递归
+        ///// </summary>
+        ///// <param name="root"></param>
+        ///// <returns></returns>
+        //public IList<IList<int>> LevelOrder(Node root)
+        //{
+        //    if (root != null) TraverseNode(root, 0);
+        //    return result;
+        //}
+
+        //private List<IList<int>> result = new List<IList<int>>();
+
+        //private void TraverseNode(Node node, int level)
+        //{
+        //    if (result.Count <= level) result.Add(new List<int>());
+
+        //    result[level].Add(node.val);
+        //    foreach (var child in node.children)
+        //    {
+        //        TraverseNode(child, level + 1);
+        //    }
+        //}
+
+        ///// <summary>
+        ///// 方法2：用两个List，
+        /////     把每层的节点放在一个List中
+        ///// </summary>
+        ///// <param name="root"></param>
+        ///// <returns></returns>
+        //public IList<IList<int>> LevelOrder(Node root)
+        //{
+        //    var result = new List<IList<int>>();
+        //    if (root == null) return result;
+
+        //    var list = new List<Node>() { root };
+        //    while (list.Count > 0)
+        //    {
+        //        var line = new List<int>();
+        //        var levelNodes = new List<Node>();
+        //        foreach(var node in list)
+        //        {
+        //            line.Add(node.val);
+        //            if(node.children!=null) levelNodes.AddRange(node.children);
+        //        }
+
+        //        result.Add(line);
+        //        list = levelNodes;
+        //    }
+        //    return result;
+        //}
+
+        /// <summary>
+        /// 方法3 : 利用数组先进先出的特性进行求解
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                int[] line = new int[size];
+
+                for(int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    line[i] = node.val;
+                    if (node.children != null)
+                    {
+                        foreach (var child in node.children)
+                            queue.Enqueue(child);
+                    }
+                }
+
+                result.Add(line);
+            }
+            return result;
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
